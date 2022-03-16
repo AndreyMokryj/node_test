047c97299f405ac25863af5012eebfbdb66e05cb
andrew <andrew_mokryj@ukr.net>
2022-03-16 16:05:46 +0200

    Initial

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const rabbitConfig = {
+    uri: process.env.rabbbitUri || 'amqp://localhost',
+    workQueue: process.env.workQueue || 'workQueue',
+}
+
+export default rabbitConfig
---
+++ src/worker/index.js
@@ -0,0 +1,31 @@
+import amqp from 'amqplib'
+import { resolve } from 'bluebird'
+import config from '../config'
+
+const assertQueueOptions = { durable: true }
+const consumeQueueOptions = { noAck: false }
+const { uri, workQueue } = config
+
+const genRamdomTime = () => Math.random() * 10000
+
+const processHeavyTask = msg => resolve(console.log('Message received'))
+    .then(setTimeout(() => console.log(msg.content.toString()), genRamdomTime()))
+
+const assertAndConsumeQueue = (channel) => {
+    console.log('Worker is running! Waiting for new messages...')
+
+    const ackMsg = (msg) => resolve(msg)
+        .tap(msg => processHeavyTask(msg))
+        .then((msg) => channel.ack(msg))
+
+    return channel.assertQueue(workQueue, assertQueueOptions)
+        .then(() => channel.prefetch(1))
+        .then(() => channel.consume(workQueue, ackMsg, consumeQueueOptions))
+}
+
+const listenToQueue = () => amqp.connect(uri)
+    .then(connection => connection.createChannel())
+    .then(channel => assertAndConsumeQueue(channel))
+
+
+export default listenToQueue()
