34f064b725696bd949ca7b9231005d373812424d
andrew <andrew_mokryj@ukr.net>
2022-03-21 17:53:04 +0200

    feature: unit tests for OCR

--- test/ocr.test.js
+++ test/ocr.test.js
@@ -1,8 +1,8 @@
 import {recognizeFromFileUrl} from "../src/worker";
 
-// test('random string as file url', () => {
-//     expect(recognizeFromFileUrl('aaa')).toBe('');
-// });
+test('random string as file url', async () => {
+    expect(await recognizeFromFileUrl('aaa')).toBe(undefined);
+});
 
 test('url of file containing text', async () => {
     expect(await recognizeFromFileUrl('https://previews.123rf.com/images/happyroman/happyroman1611/happyroman161100004/67968361-atm-transaction-printed-paper-receipt-bill-vector.jpg'))
@@ -18,5 +18,12 @@ test('url of file containing text', async () => {
             'REQUSTED AMOUNT $100.00\n' +
             'TERMINAL FEE Sik25\n' +
             '\n' +
-            'TOTAL AMOUNT $101.25\n')
+            'TOTAL AMOUNT $101.25')
+});
+
+test('url of file with no text', async () => {
+    expect((await recognizeFromFileUrl('https://media.istockphoto.com/photos/feline-picture-id108226626?k=20&m=108226626&s=612x612&w=0&h=bDaWwxhcygjerdR71qQu8YFSXaDUIi_HUZV2AADg5L8='))
+        .replaceAll("\n", '')
+        .replaceAll(" ", ''))
+        .toBe('');
 });
